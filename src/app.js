bf80a0f954028b3a3bb32423320ca7e4238c6f40
vignesh <vignesh@gmail.com>
2025-12-10 13:45:19 +0530

    securing api with arcjet

--- src/app.js
+++ src/app.js
@@ -1,9 +1,46 @@
 import express from 'express';
+import logger from './configs/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import authRoutes from './routes/auth.router.js';
+import securityMiddleware from './middlewares/security.middleware.js';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
+// Arcjet security middleware – applies shield, bot detection, and rate limiting
+app.use(securityMiddleware);
+
 app.get('/', (req, res) => {
+  logger.info('Hello from acquisition');
   res.status(200).send('Hello from acquisition');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisitions API is running!' });
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
